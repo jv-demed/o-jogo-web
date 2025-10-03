8d65f8ddcbda5b6a54f21adb906c828dbf496679
CamelComp <jv.demed@gmail.com>
2025-10-03 16:21:24 -0300

    feat: ajustes no visual do modal para cartas

--- components/containers/Modal.jsx
+++ components/containers/Modal.jsx
@@ -6,23 +6,21 @@ export function Modal({ isOpen, onClose, children }) {
 
     return (
         <div className={`
-            fixed inset-0 bg-black/50 px-4
+            fixed inset-0 bg-black/60 px-4
             flex items-center justify-center z-50
         `}>
             <div className={`
-                flex justify-center items-center
-                bg-[#212121] text-[#e2d4b8] 
-                rounded-2xl p-6 max-w-lg w-full 
+                flex flex-col justify-center items-center gap-2
+                px-6 py-1 max-w-lg w-full 
                 relative shadow-xl
             `}>
-                <button onClick={onClose} 
-                    className={`
-                        absolute top-3 right-3 
-                        text-2xl hover:text-red-400    
-                    `}
-                >
-                    <ICONS.close />
-                </button>
+                <header className='flex justify-end w-full'>
+                    <button onClick={onClose} 
+                        className={` text-2xl hover:text-red-400`}
+                    >
+                        <ICONS.close />
+                    </button>
+                </header>
                 {children}
             </div>
         </div>
