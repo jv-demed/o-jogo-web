040eb31d907e2baa767a4b28410028b826c7f8bb
CamelComp <jv.demed@gmail.com>
2025-11-19 23:57:57 -0300

    feat: adição de cartas v2

--- assets/packs.js
+++ assets/packs.js
@@ -3,5 +3,9 @@ export const PACKS = [
         id: 1,
         name: 'Amigue Secrete 2023',
         dateRealease: new Date('2023-12-22')
+    },{
+        id: 2,
+        name: 'Amigue Secrete 2024',
+        dateRealease: new Date('2024-12-20')
     }
 ];
